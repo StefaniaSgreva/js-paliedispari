e9858c3d0521a16b778be4224025dfcd7bfe9843
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-16 18:02:00 +0200

    ex.2 wip

--- JS/myLibrary.js
+++ JS/myLibrary.js
@@ -1,3 +1,9 @@
+//todo
+//!
+//*
+//?
+//
+//* PALINDROME 
 function palindrome(str) {
       // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
     let re = /[\W_]/g;
@@ -7,8 +13,32 @@ function palindrome(str) {
       // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
     return reverseStr === lowRegStr;
   }
+//* RANDOM NUMBER 
+function randomNumber(min, max) {
+  //console.log(arguments);
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+//*RANDOM SUM
+function randomSum(min, max){
+  const firstNumber = randomNumber(min, max);
+  const secondNumber = randomNumber(min, max);
+  return firstNumber + secondNumber;
+} //const sum = randomSum(100, 10000);
+//* IS EVEN ?
+function isEven(num){
+  if(num % 2 === 0){
+    return true;
+  } else{
+    return false;
+  }
+}
+//* NOTIFICATION ERROR
+function notificationError(msgError){
+  const attention = document.createElement('div');
+  attention.className = 'alert alert-danger';
+  attention.innerText = msgError;
+  return attention;
 
-
-
+}
  
  
--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,11 @@
 "use strict"; 
-/*Palidroma:
+
+/*todoPalidroma:
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
 const btnPal = document.querySelector('.btnWord');
-const result = document.querySelector('h4'); 
+const result1 = document.querySelector('.result1'); 
 
 btnPal.addEventListener('click', ()=>{
     let userWord = document.getElementById('palindrome').value;  
@@ -12,15 +13,13 @@ btnPal.addEventListener('click', ()=>{
     // console.log(palindrome(userWord));
     if (palindrome(userWord)){
 
-        result.innerHTML = 'This word is a palindrome !'; 
+        result1.innerHTML = 'This word is a palindrome !'; 
 
     }else{
 
-        result.innerHTML = 'This word is not a palindrome !';
+        result1.innerHTML = 'This word is not a palindrome !';
     }  
 });
-
-
 
 /*Pari e Dispari:
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -28,3 +27,37 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
+
+const btnEO = document.querySelector('.btnNumber');
+const inputTagEven = document.getElementById('numberEven');
+const inputTagOdd = document.getElementById('numberOdd');
+const result2 = document.querySelector('.result2'); 
+const container = document.querySelector('.containerAlert');
+
+btnEO.addEventListener('click', function(){
+    const validNumbers = [1, 2, 3, 4, 5];
+    let value = parseInt(inputTagEven.value) || parseInt(inputTagOdd.value); //Element to be searched
+    if(validNumbers.includes(value)){
+
+        function sum(){
+            const firstNumber = parseInt(inputTagEven.value) || parseInt(inputTagOdd.value);
+            const secondNumber = randomNumber(1, 5);
+            return firstNumber + secondNumber;
+          }
+          console.log(sum());
+
+        if(isEven(sum)){ //!isEven
+            result2.innerHTML = sum() + 'even';
+        } else {
+            result2.innerHTML = sum() + 'odd';
+        }
+
+    } else{
+        result2.innerHTML = ' ';
+        const divAlert = notificationError('Attention: you must enter a number from 1 to 5 !');
+        container.prepend(divAlert);
+        return;
+    }
+    const attentionToRemove = document.querySelector('.alert');
+    if(attentionToRemove) attentionToRemove.remove(); 
+});
